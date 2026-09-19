1d351e1eae99e03dd865b23ebf1a4f2d8392a763
BagusDKa <127530796+BagusDKa@users.noreply.github.com>
2026-09-19 22:20:41 +0800

    Create Excavator.java

---
+++ Excavator.java
@@ -0,0 +1,33 @@
+/**
+ * Subclass 2: Excavator (mewarisi KendaraanTambang).
+ */
+public class Excavator extends KendaraanTambang {
+ 
+    private double kapasitasBucket; // dalam m3
+ 
+    public Excavator(String id, String namaKendaraan, double hargaSewaPerHari, double kapasitasBucket) {
+        super(id, namaKendaraan, hargaSewaPerHari);
+        setKapasitasBucket(kapasitasBucket);
+    }
+ 
+    public double getKapasitasBucket() {
+        return kapasitasBucket;
+    }
+ 
+    public void setKapasitasBucket(double kapasitasBucket) {
+        if (kapasitasBucket <= 0) {
+            throw new IllegalArgumentException("Kapasitas bucket harus lebih dari 0.");
+        }
+        this.kapasitasBucket = kapasitasBucket;
+    }
+ 
+    @Override
+    public String getJenis() {
+        return "Excavator";
+    }
+ 
+    @Override
+    protected String getDetail() {
+        return String.format("Bucket %.1f m3", kapasitasBucket);
+    }
+}
